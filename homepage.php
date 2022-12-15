d330521bb5625753839a1911ed8c80eff3e5c19e
Fariz Muhammad Aditya <111466154+FMATheNomad@users.noreply.github.com>
2022-12-16 01:11:04 +0700

    Add files via upload

---
+++ homepage.php
@@ -0,0 +1,331 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Gamsahabnida! Online Kpop Store</title>
+	<link rel="stylesheet" href="style-homepage.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="fontawesome/css/all.min.css">
+</head>
+<body>
+	<!-- Navbar Start -->
+	<nav class="navbar navbar-dark navbar-expand-lg bg-dark">
+	  <div class="container">
+	     <a class="navbar-brand" href="#">
+		    <img src="assets/logo.png" alt="Logo" width="50" height="50" class="me-2"> <strong>G!</strong> Kpop Store
+		 </a>
+	    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+	      <span class="navbar-toggler-icon"></span>
+	    </button>
+	    <div class="collapse navbar-collapse" id="navbarNav">
+	    	<form class="d-flex ms-auto my-4 my-lg-0" role="search">
+		      <input class="form-control me-2" type="search" placeholder="Search..." aria-label="Search"">
+		      <button class="btn btn-outline-info" type="submit"><i class="fa-brands fa-searchengin"></i></button>
+		    </form>
+	      <ul class="navbar-nav ms-auto">
+	        <li class="nav-item">
+	          <a class="nav-link" href="#">Keranjang</a>
+	        </li>
+	        <li class="nav-item">
+	          <a class="nav-link" href="#">Notifikasi</a>
+	        </li>
+	        <li class="nav-item">
+	          <a class="nav-link" href="#">Bantuan</a>
+	        </li>
+	        <li class="nav-item">
+	          <a class="nav-link" href="#">Daftar</a>
+	        </li>
+	        <li class="nav-item">
+	          <a class="nav-link" href="#">Masuk</a>
+	        </li>
+	      </ul>
+	    </div>
+	  </div>
+	</nav>
+	<!-- Navbar End -->
+	<!-- Carousel Start -->
+	<div class="container">
+		<div id="carouselExampleIndicators" class="carousel slide mt-5" data-bs-ride="true">
+		  <div class="carousel-indicators">
+		    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+		    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+		    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+		    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+		    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
+		    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="5" aria-label="Slide 6"></button>
+		  </div>
+		  <div class="carousel-inner">
+		    <div class="carousel-item active">
+		      <img src="assets/GI-DLE.jpg" class="d-block img-fluid" alt="GI-DLE">
+		    </div>
+		    <div class="carousel-item">
+		      <img src="assets/ITZY.jpg" class="d-block img-fluid" alt="Itzy">
+		    </div>
+		    <div class="carousel-item">
+		      <img src="assets/Red Velvet.jpg" class="d-block img-fluid" alt="Red Velvet">
+		    </div>
+		    <div class="carousel-item">
+		    	<img src="assets/TXT.jpg" class="d-block img-fluid" alt="Tomorrow X Together">
+		    </div>
+		    <div class="carousel-item">
+		    	<img src="assets/Stray Kids.jpg" class="d-block img-fluid" alt="Stray Kids">
+		    </div>
+		    <div class="carousel-item">
+		    	<img src="assets/OneUs.jpg" class="d-block img-fluid" alt="OneUs">
+		    </div>
+		  </div>
+		  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+		    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+		    <span class="visually-hidden">Previous</span>
+		  </button>
+		  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+		    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+		    <span class="visually-hidden">Next</span>
+		  </button>
+		</div>
+	</div>
+	<!-- Carousel End -->
+	<!-- Category Start -->
+	<div class="container mt-5">
+		<div class="title text-center">
+			<h5><u>Categories</u></h5>
+		</div>
+		<div class="row text-center mt-2">
+			<div class="col-lg-2 col-md-3 col-sm-4 col-6">
+				<div class="menu-kategori">
+					<a href=""><img src="assets/kategori/CD.png" alt="" style="width: 50px;" class="mt-3"></a>
+					<p class="mt-2">CD</p>
+				</div>
+			</div>
+			<div class="col-lg-2 col-md-3 col-sm-4 col-6">
+				<div class="menu-kategori">
+					<a href=""><img src="assets/kategori/DVD.png" alt="" style="width: 50px;" class="mt-3"></a>
+					<p class="mt-2">DVD</p>
+				</div>
+			</div>
+			<div class="col-lg-2 col-md-3 col-sm-4 col-6">
+				<div class="menu-kategori">
+					<a href=""><img src="assets/kategori/KDrama.png" alt="" style="width: 50px;" class="mt-3"></a>
+					<p class="mt-2">KDrama</p>
+				</div>
+			</div>
+			<div class="col-lg-2 col-md-3 col-sm-4 col-6">
+				<div class="menu-kategori">
+					<a href=""><img src="assets/kategori/KIT Video.png" alt="" style="width: 50px;" class="mt-3"></a>
+					<p class="mt-2">KIT Video</p>
+				</div>
+			</div>
+			<div class="col-lg-2 col-md-3 col-sm-4 col-6">
+				<div class="menu-kategori">
+					<a href=""><img src="assets/kategori/Korean Beauty.png" alt="" style="width: 50px;" class="mt-3"></a>
+					<p class="mt-2">Korean Beauty</p>
+				</div>
+			</div>
+			<div class="col-lg-2 col-md-3 col-sm-4 col-6">
+				<div class="menu-kategori">
+					<a href=""><img src="assets/kategori/Korean Food.png" alt="" style="width: 50px;" class="mt-3"></a>
+					<p class="mt-2">Korean Food</p>
+				</div>
+			</div>
+			<div class="col-lg-2 col-md-3 col-sm-4 col-6">
+				<div class="menu-kategori">
+					<a href=""><img src="assets/kategori/Kpop Fashion.png" alt="" style="width: 50px;" class="mt-3"></a>
+					<p class="mt-2">Kpop Fashion</p>
+				</div>
+			</div>
+			<div class="col-lg-2 col-md-3 col-sm-4 col-6">
+				<div class="menu-kategori">
+					<a href=""><img src="assets/kategori/Kpop Stuff.png" alt="" style="width: 50px;" class="mt-3"></a>
+					<p class="mt-2">Kpop Stuff</p>
+				</div>
+			</div>
+			<div class="col-lg-2 col-md-3 col-sm-4 col-6">
+				<div class="menu-kategori">
+					<a href=""><img src="assets/kategori/LightStick.png" alt="" style="width: 50px;" class="mt-3"></a>
+					<p class="mt-2">LightStick</p>
+				</div>
+			</div>
+			<div class="col-lg-2 col-md-3 col-sm-4 col-6">
+				<div class="menu-kategori">
+					<a href=""><img src="assets/kategori/Official Merchandise.png" alt="" style="width: 50px;" class="mt-3"></a>
+					<p class="mt-2">Official Merchandise</p>
+				</div>
+			</div>
+			<div class="col-lg-2 col-md-3 col-sm-4 col-6">
+				<div class="menu-kategori">
+					<a href=""><img src="assets/kategori/Photobook & Magazine.png" alt="" style="width: 50px;" class="mt-3"></a>
+					<p class="mt-2">Photobook & Magazine</p>
+				</div>
+			</div>
+			<div class="col-lg-2 col-md-3 col-sm-4 col-6">
+				<div class="menu-kategori">
+					<a href=""><img src="assets/kategori/Season Greetings.png" alt="" style="width: 50px;" class="mt-3"></a>
+					<p class="mt-2">Season Greetings</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Category End -->
+	<!-- Showcase Product Start -->
+	<div class="container mt-5">
+		<div class="title text-center">
+			<h5><u>Products</u></h5>
+		</div>
+		<div class="row">
+			<div class="col-lg-2 col-md-2 col-sm-4 col-6 mt-3">
+				<div class="card text-center">
+				  <img src="assets/produk/produk1.jpg" class="card-img-top" alt="...">
+				  <div class="card-body">
+				    <h6 class="card-title">Headset</h6>
+				    <p class="card-text">Rp. 1000,-</p>
+				    <a href="#" class="btn btn-primary d-grid">Lihat detail</a>
+				  </div>
+				</div>
+			</div>
+			<div class="col-lg-2 col-md-2 col-sm-4 col-6 mt-3">
+				<div class="card text-center">
+				  <img src="assets/produk/produk2.jpg" class="card-img-top" alt="...">
+				  <div class="card-body">
+				    <h6 class="card-title">Baju</h6>
+				    <p class="card-text">Rp. 1000,-</p>
+				    <a href="#" class="btn btn-primary d-grid">Lihat detail</a>
+				  </div>
+				</div>
+			</div>
+			<div class="col-lg-2 col-md-2 col-sm-4 col-6 mt-3">
+				<div class="card text-center">
+				  <img src="assets/produk/produk3.jpg" class="card-img-top" alt="...">
+				  <div class="card-body">
+				    <h6 class="card-title">Sepatu</h6>
+				    <p class="card-text">Rp. 1000,-</p>
+				    <a href="#" class="btn btn-primary d-grid">Lihat detail</a>
+				  </div>
+				</div>
+			</div>
+			<div class="col-lg-2 col-md-2 col-sm-4 col-6 mt-3">
+				<div class="card text-center">
+				  <img src="assets/produk/produk4.jpg" class="card-img-top" alt="...">
+				  <div class="card-body">
+				    <h6 class="card-title">Jam Tangan</h6>
+				    <p class="card-text">Rp. 1000,-</p>
+				    <a href="#" class="btn btn-primary d-grid">Lihat detail</a>
+				  </div>
+				</div>
+			</div>
+			<div class="col-lg-2 col-md-2 col-sm-4 col-6 mt-3">
+				<div class="card text-center">
+				  <img src="assets/produk/produk5.jpg" class="card-img-top" alt="...">
+				  <div class="card-body">
+				    <h6 class="card-title">Hand Sanitizer</h6>
+				    <p class="card-text">Rp. 1000,-</p>
+				    <a href="#" class="btn btn-primary d-grid">Lihat detail</a>
+				  </div>
+				</div>
+			</div>
+			<div class="col-lg-2 col-md-2 col-sm-4 col-6 mt-3">
+				<div class="card text-center">
+				  <img src="assets/produk/produk6.jpg" class="card-img-top" alt="...">
+				  <div class="card-body">
+				    <h6 class="card-title">Tas</h6>
+				    <p class="card-text">Rp. 1000,-</p>
+				    <a href="#" class="btn btn-primary d-grid">Lihat detail</a>
+				  </div>
+				</div>
+			</div>
+			<div class="col-lg-2 col-md-2 col-sm-4 col-6 mt-3">
+				<div class="card text-center">
+				  <img src="assets/produk/produk7.jpg" class="card-img-top" alt="...">
+				  <div class="card-body">
+				    <h6 class="card-title">Kamera</h6>
+				    <p class="card-text">Rp. 1000,-</p>
+				    <a href="#" class="btn btn-primary d-grid">Lihat detail</a>
+				  </div>
+				</div>
+			</div>
+			<div class="col-lg-2 col-md-2 col-sm-4 col-6 mt-3">
+				<div class="card text-center">
+				  <img src="assets/produk/produk8.jpg" class="card-img-top" alt="...">
+				  <div class="card-body">
+				    <h6 class="card-title">Celana</h6>
+				    <p class="card-text">Rp. 1000,-</p>
+				    <a href="#" class="btn btn-primary d-grid">Lihat detail</a>
+				  </div>
+				</div>
+			</div>
+			<div class="col-lg-2 col-md-2 col-sm-4 col-6 mt-3">
+				<div class="card text-center">
+				  <img src="assets/produk/produk9.jpg" class="card-img-top" alt="...">
+				  <div class="card-body">
+				    <h6 class="card-title">Keyboard</h6>
+				    <p class="card-text">Rp. 1000,-</p>
+				    <a href="#" class="btn btn-primary d-grid">Lihat detail</a>
+				  </div>
+				</div>
+			</div>
+		</div>
+	</div>	
+	<!-- Showcase Product End -->
+	<!-- Footer Start -->
+	<footer id="footer">
+		<div class="footer-center" id="pavo-footer-center">
+			<div class="container">
+				<div class="inner">
+					<div class="row">
+						<div class="moveR col-lg-3 col-md-3 col-sm-12 col-xs-12">
+							<div class="box pav-custom">
+								<div class="custom-content">
+									<div class="box-heading"><span>Korean Chart</span>
+									</div>
+									<p><span style="font-size: 10pt;">Semua pembelian Album fisik di Gamsahabnida! Online Kpop Store akan terhitung di <strong>HANTEO &amp; GAON Chart</strong></span></p>
+									<p><img src="assets/vsBRHXI.png" alt="" width="196" height="237"></p>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 ">
+							<div class="box pav-custom  ">
+								<div class="custom-content">
+								<div class="box-heading"><span class="title">Tentang Kami</span></div>
+								<div class="content">
+									<p><strong>WhatsApp</strong>&nbsp;+6281320342023<br><a href="https://api.whatsapp.com/send?phone=6281320342023" target="_blank" rel="noopener"><span face="verdana, geneva, sans-serif" style="font-family: verdana, geneva, sans-serif;"></span></a></p>
+									<p><span style="font-size: 10pt;"><strong>Jam Kerja</strong></span><br><span style="font-size: 10pt;">Senin - Jum'at 07.30 - 11.30, 12.30 - 14.30, 15.30 - 17.30 WIB</span></p><p><span style="font-size: 8pt;"><em>Gamsahabnida! Online Kpop Store is a trusted Online Shopping Sites&nbsp;that provides imported and local Korean goodies since 2022</em></span></p></div>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 ">
+							<div class="box pav-custom  ">
+								<div class="custom-content">
+									<div class="box-heading"><span class="title">Pembayaran</span></div>
+									<sl>
+									<div class="content">
+									<div style="line-height: 2;"><img loading="lazy" src="assets/bri.png" width="110" height="38" class="img-payment" alt="BRI"></div>
+									</ul>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+							<div class="box footerlink">
+								<div class="box-heading">
+									<span>Information</span>
+									<div class="content">
+									<ul class="list">
+										<a href="/content/Cara-Order"><span class="menu-title">Cara Order</span></a>
+										<br>
+										<a href="/konfirmasipembayaran"><span class="menu-title">Konfirmasi Pembayaran</span></a>
+										<br>
+										<a href="/contactus.php"><span class="menu-title">Kontak Kami</span></a>
+									</ul>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>	
+		</div>
+	</footer>
+	<!-- Footer End -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
