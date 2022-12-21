ce7be39f8ff5271be559980116dc7e5f77862f55
Fariz Muhammad Aditya <111466154+FMATheNomad@users.noreply.github.com>
2022-12-21 11:38:12 +0700

    update

--- homepage.php
+++ homepage.php
@@ -54,11 +54,11 @@
 		    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
 		    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
 		    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="5" aria-label="Slide 6"></button>
-		    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="5" aria-label="Slide 7"></button>
+		    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="6" aria-label="Slide 7"></button>
 		  </div>
 		  <div class="carousel-inner">
-		    <div class="carousel-itema active">
-		    	<img src="assets/G!.jpeg" class="d-block img-fluid" alt="Gamsahabnida!">
+		    <div class="carousel-item active">
+		    	<img src="assets/photo1671596642.jpeg" class="d-block img-fluid" alt="Gamsahabnida!">
 		    </div>
 		    <div class="carousel-item">
 		      <img src="assets/GI-DLE.jpg" class="d-block img-fluid" alt="GI-DLE">
